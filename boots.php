4d6026b036febcb626e5e87f5fbf4b1b1951f291
bmgowtham <gowthambm93@gmail.com>
2016-08-27 13:49:52 +0530

    Add files via upload

---
+++ boots.php
@@ -0,0 +1,134 @@
+<html>
+	<head>
+		<title>Welcome To Webpage</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="boot.css">
+        <script src="jquery-3.1.0.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+	</head>
+			<div class="container">
+              <br>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+             <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+             </ol>
+            <div class="carousel-inner" role="listbox">
+               <div class="item active">
+                  <img src="fashion.jpg" alt="Chania" width="40%" height="50%">
+               </div>
+               <div class="item">
+                  <img src="header2.jpg" alt="Chania" width="40%" height="60%">
+               </div>
+              <div class="item">
+                  <img src="watchs.jpg" alt="Flower" width="40%" height="50%">
+              </div>
+            </div>
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+         </a>
+             </div>
+            </div>
+           </div>
+           <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Cilents</a></li>
+        <li><a href="#">Products</a></li> 
+        <li><a href="#">Contacts</a></li> 
+      </ul>
+    </div>
+  </div>
+</nav>
+
+			<div class="row">
+            <div class="col-sm-3">
+				<div id="lside">
+					<img src="dress.jpg" width="100%" height="50%">
+					<img src="watch.jpg" width="100%" height="50%">
+				</div>
+            </div>
+           <div class="col-sm-2">
+				<div id="center">
+					<div id="add1">
+						<img src="camera.jpg" width="100%" height="50%">
+                        <div id="il">
+							<i><b><h5 align="center">Canon Camera</h5></b></i>
+							<i>Amount:<strike>24500</strike><br><strike>22500</strike>   53%   18499</i>
+                        </div>
+					</div>
+					<div id="add1">
+						<img src="dre.jpg" width="100%" height="50%">
+						<div id="il">
+							<i><b><h5 align="center">Red Shirts</h5></b></i>
+							<i>Amount:<strike>4500</strike><br><strike>2200 </strike>   64%   1099</i>
+                        </div>
+					</div>
+					</div>
+					</div>
+					<div class="col-sm-2">
+				<div id="center">
+					<div id="add1">
+						<img src="silver.jpeg" width="100%" height="50%">
+						<div id="il">
+							<i><b><h5 align="center">Silver Combo Pack</h5></b></i>
+							<i>Amount:<strike>5500</strike><br><strike>2500 </strike>   23%   1999</i>
+                        </div>
+					</div>
+					<div id="add1">
+						<img src="tawel1.jpg" width="100%" height="50%">
+						<div id="il">
+							<i><b><h5 align="center">Tawels</h5></b></i>
+							<i>Amount:<strike>1000</strike><br><strike>800 </strike>   66%  499</i>
+                        </div>
+     				</div>
+     				</div>
+     				</div>
+     				<div class="col-sm-2">
+				    <div id="center">
+     				<div id="add1">
+						<img src="system.jpg" width="100%" height="50%">
+						<div id="il">
+							<i><b><h5 align="center">DELL</h5></b></i>
+							<i>Amount:<strike>43500</strike><br><strike>39500 </strike>   43%  33499</i>
+                        </div>
+					</div>
+					<div id="add1">
+						<img src="book.jpg" width="100%" height="50%">
+						<div id="il">
+							<i><b><h5 align="center">Book</h5></b></i>
+							<i>Amount:<strike>1000</strike><br><strike>800 </strike>   53%  499</i>
+                        </div>
+     				</div>
+           </div>
+            </div>
+            <div class="col-sm-3">
+				<div id="lside">
+					<img src="jeans.jpg" width="100%" height="50%">
+					<img src="class.jpg" width="100%" height="50%">
+				</div>
+            </div>
+		<div class="col-sm-12">
+			<div id="foot">
+				Online Shooping 2016 @ www.onlineShooping.com
+			</div>
+		</div>
+	</div>
+</html>
